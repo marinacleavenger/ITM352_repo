0f2c6260b0dbc506434706913305bf1201892c0b
Marina Cleavenger <marinacl@hawaii.edu>
2023-09-26 00:34:44 -1000

    lab6update

--- lab6extra/lab6d.js
+++ lab6extra/lab6d.js
@@ -3,12 +3,12 @@ let monthInput=document.getElementById("monthInput");
 let result=document.getElementById("result");
 //Only when button is selected will the function run
 document.querySelector('button').addEventListener('click', function () {
-let month=monthInput.value.toLowerCase();
+    let month=monthInput.value.toLowerCase();
 
 //default value also can be an error on the input 
     let num_days =-1;
     //switch is introduced to change the output dependent on what the input is. This can also match expressions. Case will execute the block of code. 
-    switch(month){
+    switch(month) {
         case "january":
         case "march":
         case "may":
@@ -16,16 +16,20 @@ let month=monthInput.value.toLowerCase();
         case "august":
         case "october":
         case "december":
-        num_days =31;
+            num_days=31;
             break;
         case "april":
         case "june":
         case "september":
         case "november":
-        num_days=30;
-        break;
+            num_days=30;
+            break;
         case "february":
-        num_days=28;
+            num_days=28;
+            break;
+        default:
+            num_days=-1;
+            break;
     }
 //if it is not spelled correctly it will say invalid, but if not the switch will comense and you will have the days of the month.
     if (num_days === -1){
