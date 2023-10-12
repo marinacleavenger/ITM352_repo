eacb2bcf948a54ee161a45cc7f06a8fa1d2bf1eb
Marina Cleavenger <marinacl@hawaii.edu>
2023-10-12 14:38:44 -0700

    lab8

---
+++ Lab8/Invoice1_1/lab8ex3.js
@@ -0,0 +1,35 @@
+// define the product quantites array 
+let product_quantities= [2, 1, 1, 1, 1];
+
+// array of all products
+// corresponds to product_quantities array
+// product_quantities[i] is the quantity for products[i]
+let products = [
+    { 'name': 'small gumball', 'price': 0.02 },
+    { 'name': 'medium gumball', 'price': 0.05 },
+    { 'name': 'large gumball', 'price': 0.07 },
+    { 'name': 'small jawbreaker', 'price': 0.06 },
+    { 'name': 'large jawbreaker', 'price': 0.10 }
+   ];
+
+//prodct_quantities and print each element in table
+document.write("<table>");
+//lab 8 part 3.2
+document.write("<tr><th>Product #</th><th>Name</th><th>Price</th><th>Quantity</th><th>Extended Price</th></tr>");
+
+// document.write("<tr><th> Product #</th><th>Quantity</th></tr>");
+
+for (let i=0; i < product_quantities.length; i++) {
+    let quantity=product_quantities[i];
+    let product=products[i];
+    let extended_cost = quantity * product.price;
+
+    document.write("<tr>");
+    document.write("<td>" + (i+1)+ "</td>"); //product#
+    document.write("<td>" + product.name + "</td>"); //name 
+    document.write("<td>" + product.price.toFixed(2) + "</td>"); //price 
+    document.write("<td>" + quantity + "</td>"); //quantity
+    document.write("<td>" + extended_cost.toFixed(2) + "</td>"); //Extend cost   
+    document.write("/<tr>");
+}
+document.write("</table>");
